526760148c58bd79ecf218186faba8fbe97e80ba
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-07-18 15:43:11 +0600

    Complete the result summary component

---
+++ scripts.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const circle = document.getElementById('circle');
+  const width = circle.clientWidth;
+  circle.style.height = width + 'px';
+});
